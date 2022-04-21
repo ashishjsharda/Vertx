79595914ef8b07848b51e808030ba336281a8c71
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-21 07:01:18 -0500

    Add start and logger

--- src/main/java/HelloVertx.java
+++ src/main/java/HelloVertx.java
@@ -1,2 +1,12 @@
-public class HelloVertx {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+import java.util.logging.Logger;
+
+public class HelloVertx extends AbstractVerticle {
+
+    public void start(Future<Void> future){
+        Logger.getLogger(HelloVertx.class.getName()).info("Getting Started with Vertx");
+
+    }
 }
